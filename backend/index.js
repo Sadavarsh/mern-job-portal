8dded967977c6d4ea67432063e8032974721daa1
Sadavarsh <sadavarshsaini1@gmail.com>
2024-12-23 09:54:17 -0800

    update url

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://mern-job-portal-mauve.vercel.app',
+    origin:'http://localhost:5173',
     credentials:true
 }
 
