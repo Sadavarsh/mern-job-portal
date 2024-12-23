36ec6d02cb4d44f6013fa2aedb1bc941bdc97d4c
Sadavarsh <sadavarshsaini1@gmail.com>
2024-12-23 06:05:29 -0800

    update url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://mern-job-portal-kappa.vercel.app/',
+    origin:'https://mern-job-portal-uj55.onrender.com',
     credentials:true
 }
 
